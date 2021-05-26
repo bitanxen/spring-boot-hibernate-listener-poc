91912faf4f0007ced80d6f2553ac59c793ca7c42
Bitan Biswas <bitanxen@gmail.com>
2021-05-26 17:46:13 +0800

    minor change:

--- src/main/java/com/sysbean/test/HibernateListenerPocApplication.java
+++ src/main/java/com/sysbean/test/HibernateListenerPocApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HibernateListnerPocApplication {
+public class HibernateListenerPocApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HibernateListnerPocApplication.class, args);
+		SpringApplication.run(HibernateListenerPocApplication.class, args);
 	}
 
 }
